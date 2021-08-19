bbb02ddedc972111c72bcd561ca86e0682b1a4b5
Vickie Hughes <vickie_hughes@hotmail.com>
2021-08-18 22:25:19 -0500

    add tableau

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
+from waitress import serve 
 from flask import Flask, current_app
-
 
 
 
 app = Flask(__name__)
 
+if __name__ == "__main__":
+    serve(app)
+
 @app.route("/")
-def hello_world():
-    # return "<p>Hello, World!</p>"
+def mhomepage():
     return current_app.send_static_file('pages/index.html')
+
+@app.route("/tableau")
+def my_tableau_page():
+    # return "<p>Hello, World!</p>"
+    return current_app.send_static_file('pages/tableautest.html')
 
 
